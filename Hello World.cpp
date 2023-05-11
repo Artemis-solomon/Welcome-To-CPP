09fd0b93173beee175fec431a9abb7e328fd2749
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-11 19:27:39 -0400

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,56 @@
+//Artemis Solomon
+//Welcome to C++: Hello World
+//Example 1
+
+//Header File Library
+#include <iostream>
+
+//Function
+int main()
+{
+  //std:: indicates we are using the Standard Library.
+  //cout (see-out) is the Object
+  // << is the insertion operator
+  std::cout << "Hello World!";
+  //This ends the Function
+  return 0;
+}
+------------------------------------------------------------
+//////////\\\\\\\\\\//////////\\\\\\\\\\//////////\\\\\\\\\\
+------------------------------------------------------------
+
+ //This is a very, very basic program. 
+ //Below is several examples of how the code could be written alternatively
+  
+ //Example 2
+  
+  #include <iostream>
+  
+  using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  return ;
+}
+------------------------------------------------------------
+ //Example 3
+  
+  #include <iostream>
+  #include <iomanip>
+  
+  int main ()
+{
+    std::cout << std::setw(10) << "Hello" "World!";
+    return 0;
+}
+------------------------------------------------------------
+  //Example 4
+  
+  #include <iostream>
+  
+  int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
