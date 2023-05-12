c223f204e32f9a3dbfc34940eaf1d5ecb3b5cd8d
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-11 20:14:09 -0400

    Create Output.cpp

---
+++ Output.cpp
@@ -0,0 +1,42 @@
+//Welcome To C++: Output
+//Output is when the program "prints" data to the console "screen".
+//Example 1:
+
+//Header File Library
+#include <iostream>
+
+//Function
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "I am writing code!" << std::endl;
+  std::cout << "You can do this infinetly" << std::endl;
+  return 0;
+}
+//If you ran this program the computer would print:
+//Hello World!
+//I am writing code!
+//You can do this infinetly
+
+//Example 2:
+//Header File Library
+#include <iostream>
+
+//Main Function
+int main()
+{
+  std::cout << "Hello World! \n";
+  std::cout << "Hello World! \n\n";
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello World!";
+  return 0;
+}
+//If you ran Example 2, the computer would print:
+//Hello World! 
+//Hello World! 
+//
+//Hello World!
+//Hello World!
+// \n creates is like hitting enter on the keyboard when you are writting.
+// \n\n create a line break
+// endl does the same as \n
