268529e164b2b341df2ab94efe1c30f6d9dccdb5
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-11 21:09:33 -0400

    Create Hello_World_Exercise.cpp
    Your very first C++ excercise! Complete the exercise and move onto the next lesson "Output".

---
+++ Hello_World_Exercise.cpp
@@ -0,0 +1,69 @@
+//Below are 5 different exercises.
+//Exercise 1:
+//Instructions: Place the [#] and [<>] signs where they go. 
+
+//Header File Library
+include iostream
+
+//Main Function
+int main()
+{
+  std::cout << "Hello World!";
+  return 0;
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+//Exercise 2:
+//Instructions: The Main Function is missing [int]. Please correct the code.
+
+//Header File Library
+#include <iostream>
+
+//Main Function
+main()
+{
+  std::cout << "Hello World!";
+  return 0;
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+//Exercise 3:
+//Instructions: This code is missing a comment, a semi colon[;] and has incorrect an incorrect Operator [''] instead of [""]. Please Correct the Code
+
+//Header File Library
+#include <iostream>
+
+//
+int main()
+{
+  std::cout << 'Hello World'
+  return 0;
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+//Exercise 4:
+//Instructions: This code is missing comments, Header File Library, an Object, Output and return integer. Please correct the Code.
+
+//
+#include <>
+
+//
+int main()
+{
+  std::  << "";
+  return ;
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+//Exercise 5:
+//Instructions: Write the code and use endl to flush the ouput buffer and start a new line.
+  
+//Header File Library
+  
+//Main Function
+  
+  
+  
+  
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
