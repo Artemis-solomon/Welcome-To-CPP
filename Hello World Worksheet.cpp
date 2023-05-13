0ef24dbc6ac67ae960ca81f39c5a9c336dc233b8
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-13 17:20:35 -0400

    Create Hello World Worksheet.cpp

---
+++ Hello World Worksheet.cpp
@@ -0,0 +1,46 @@
+//It is time to put what you just learned to use!
+//Follow the directions below to complete the worksheet.
+
+
+//Exercise 1: Hello World
+//On Line 14 between the Parentheses type in Hello World!
+
+//Header File Library
+#include <iostream>
+
+//Main Function
+int main()
+{
+  std::cout << ""; 
+  return 0;
+}
+
+//Exercise 2: My name is
+//on line 28 between the parentheses type in "my name is <insert your name>."
+
+//Header File Library
+#include <iostream>
+
+//Main Function 
+int main()
+{
+  std::cout << "Hello World!";
+  std::cout << "";
+  return 0;
+} 
+  
+  
+ //Exercise 3: Blank Canvas
+ //On lines 43, 44, and 45 insert your own text in between the parentheses.
+
+//Header File Library
+#include <iostream>
+
+//Main Function
+int main()
+{
+  std::cout << "";
+  std::cout << "";
+  std::cout << "";
+  return 0;
+}
