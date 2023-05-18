b5ce88e537f46be62cc83b910e82d863748f1a3a
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-17 21:53:53 -0400

    Create Hello_World_Worksheet_Answers.cpp

---
+++ Hello_World_Worksheet_Answers.cpp
@@ -0,0 +1,45 @@
+//Written by: Artemis Solomon
+//Welcome to C++: Hello World Worksheet Answers
+
+//Exercise 1: Hello World
+//On Line 14 between the Parentheses type in Hello World!
+
+//Header File Library
+#include <iostream>
+
+//Main Function
+int main()
+{
+  std::cout << "Hello World!"; //Answer
+  return 0;
+}
+
+//Exercise 2: My name is
+//on line 28 between the parentheses type in "my name is <insert your name>."
+
+//Header File Library
+#include <iostream>
+
+//Main Function 
+int main()
+{
+  std::cout << "Hello World!";
+  std::cout << "My name is Artemis"; //Answer
+  return 0;
+} 
+
+
+ //Exercise 3: Blank Canvas
+ //On lines 43, 44, and 45 insert your own text in between the parentheses.
+
+//Header File Library
+#include <iostream>
+
+//Main Function
+int main()
+{
+  std::cout << "Hello World!"; //Answers may vary
+  std::cout << "My name is Artemis."; //Answers may vary
+  std::cout << "My favorite color is Orange!"; //Answer may vary
+  return 0;
+}
