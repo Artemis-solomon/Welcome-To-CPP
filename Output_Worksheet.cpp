9648838c82545de439880283ba44abc74a4d6cd1
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-17 21:42:50 -0400

    Create Output_Worksheet.cpp

---
+++ Output_Worksheet.cpp
@@ -0,0 +1,39 @@
+//Exercise 1:
+//What do this program output to the console
+
+#include <iostream> //Header File Library
+
+int main() //Main Function
+
+{ //curly brace
+
+std::cout << "Hello World!"; //std is using the Standard Library
+
+return 0; //Program returns an integer (number) 0 when program is executed completely
+
+} //curly brace
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Exercise 2:
+//Insert the correct operator into the blank space
+
+#include <iostream>
+
+int main()
+{
+  std::_____ << "Hello World!";
+  return 0;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Exercise 3:
+//Write a program that outputs your first name
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "My Name is <INSERT YOUR NAME HERE>";
+  return 0;
+}
