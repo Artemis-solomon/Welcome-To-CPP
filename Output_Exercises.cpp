adb9d6fb048232de9927d866ea2c99e62d1dd287
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-17 21:49:02 -0400

    Create Output_Exercises.cpp

---
+++ Output_Exercises.cpp
@@ -0,0 +1,49 @@
+//For this exercise you will be writing the entire program.
+//Use the same steps from "Hello World!" module.
+
+//Exercise 1:
+//Write a program that outputs your name and your favorite color.
+//Use the operator endl to create a new line.
+
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+
+
+//Exercise 2:
+//Write a program that outputs a paragraph.
+//use endl to format the paragraph into 4 lines.
+
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+
+//Exercise 3:
+//Write a variation of "Hello World!" Practice the different ways to perform outputs.
+
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
+//code goes here
