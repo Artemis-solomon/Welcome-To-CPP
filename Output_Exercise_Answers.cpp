dc03f39dca3430b074a09969d098b148cc98ec22
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-17 22:08:06 -0400

    Create Output_Exercise_Answers.cpp

---
+++ Output_Exercise_Answers.cpp
@@ -0,0 +1,46 @@
+//Written by: Artemis Solomon
+//Welcome to C++: Output
+
+//For this exercise you will be writing the entire program.
+//Use the same steps from "Hello World!" module.
+//Answers will obviously vary but should be written in the same structure
+
+//Exercise 1:
+//Write a program that outputs your name and your favorite color.
+//Use the operator endl to create a new line.
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "My name is Artemis!" << std::endl;
+  std::cout << "My favorite color is Orange.";
+  return 0;
+}
+
+
+//Exercise 2:
+//Write a program that outputs a paragraph.
+//use endl to format the paragraph into 4 lines.
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, my name is Artemis." << std::endl;
+  std::cout << "I am learning how to write programs in C++!" << std::endl;
+  std::cout << "I have learned how to write my very first program already!" << std::endl;
+  std::cout << "I wonder what I will get to learn next?";
+  return 0;
+}
+
+//Exercise 3:
+//Write a variation of "Hello World!" Practice the different ways to perform outputs.
+
+#include <iostream>
+
+int main()
+{
+  std::cout "Hello" "World" "!";
+  return 0;
+}
