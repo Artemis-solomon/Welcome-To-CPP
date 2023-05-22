0b5fedd192ba8654d76ef67457cf54851b58a565
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-22 13:10:23 -0400

    Create Comments_Worksheet_Answers.cpp

---
+++ Comments_Worksheet_Answers.cpp
@@ -0,0 +1,50 @@
+//Written by: Artemis Solomon
+//Welcome to C++: Comments Worksheet Answers
+
+//Exercise 1:
+//Write the comments for the Header File Library
+
+#include <iostream> //HEADER FILE LIBRARY
+
+int main() //Main Function
+
+{ //curly brace
+
+std::cout << "Hello World!"; //std is using the Standard Library
+
+return 0; //Program returns an integer (number) 0 when program is executed completely
+
+} //curly brace
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Exercise 2:
+//Insert the comments for this code
+
+#include <iostream> //HEADER FILE LIBRARY
+
+//MAIN FUNCTION
+int main()
+{
+  std::cout << "Hello World!"; //STD IS USING THE STANDARD LIBRARY
+  return 0; //PROGRAM RETURNS AN INTEGER 0 WHEN THE PROGRAM IS EXECUTED COMPLETELY
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Exercise 3:
+//Insert the Comments for this program and write a comment on line 49 of what the output of this program will be
+
+//INSERT COMMENTS HERE
+#include <iostream>
+
+int main() //INSERT COMMENTS HERE
+{
+  std::cout << "Hello World!" << std::endl; //STD IS USING THE STANDARD LIBRARY
+  std::cout << "My Name is Artemis"; //STD IS USING THE STANDARD LIBRARY
+  return 0; //PROGRAM RETURNS AN INTEGER 0 WHEN THE PROGRAM IS EXECUTED COMPLETELY
+}
+//HELLO WORLD!
+//MY NAME IS ARTEMIS
