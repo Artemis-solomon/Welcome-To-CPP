ff57b75916158459ee3789c49a19c34f1419e7c2
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-05-22 13:07:09 -0400

    Create Comments_Worksheet.cpp

---
+++ Comments_Worksheet.cpp
@@ -0,0 +1,49 @@
+//Written by: Artemis Solomon
+//Welcome to C++: Comments Worksheet
+
+//Exercise 1:
+//Write the comments for the Header File Library
+
+#include <iostream> //INSERT COMMENTS HERE
+
+int main() //Main Function
+
+{ //curly brace
+
+std::cout << "Hello World!"; //std is using the Standard Library
+
+return 0; //Program returns an integer (number) 0 when program is executed completely
+
+} //curly brace
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Exercise 2:
+//Insert the comments for this code
+
+#include <iostream> //INSERT COMMENTS HERE
+
+//INSERT COMMENTS HERE
+int main()
+{
+  std::_____ << "Hello World!"; //INSERT COMMENTS HERE
+  return 0; //INSERT COMMENTS HERE
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Exercise 3:
+//Insert the Comments for this program and write a comment on line 49 of what the output of this program will be
+
+//INSERT COMMENTS HERE
+#include <iostream>
+
+int main() //INSERT COMMENTS HERE
+{
+  std::cout << "Hello World!" << std::endl; //INSERT COMMENTS HERE
+  std::cout << "My Name is <INSERT YOUR NAME HERE>"; //INSERT COMMENTS HERE
+  return 0; //INSERT COMMENTS HERE
+}
+//INSERT COMMENTS HERE
